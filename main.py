472243779c71aa52488cfcf9524a59eecd091fd7
Alone0506 <90964498+Alone0506@users.noreply.github.com>
2023-05-18 16:32:56 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-# https://gcyml.github.io/2019/03/03/Python%E7%88%AC%E5%8F%96Leetcode%E9%A2%98%E7%9B%AE%E5%8F%8AAC%E4%BB%A3%E7%A0%81/
-# https://github.com/eddgr/clip-leetcode/blob/master/source/main.js#L105
 import requests, json
 
 URL = 'https://leetcode.com/problems/two-sum/'
